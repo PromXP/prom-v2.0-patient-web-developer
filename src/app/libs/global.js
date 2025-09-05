649c693272754f88b05df5130fed8714781f3dfe
Inbasekar <inbatech2024@gmail.com>
2025-09-05 11:36:52 +0530

    api changed

--- src/app/libs/global.js
+++ src/app/libs/global.js
@@ -1,2 +1,2 @@
 // export const API_URL = 'https://prom-v2-0-api-developer.onrender.com/';
-export const API_URL = 'fhir-api-develop-g7h7e9hmfec3cte7.centralindia-01.azurewebsites.net/';
+export const API_URL = 'https://fhir-api-develop-g7h7e9hmfec3cte7.centralindia-01.azurewebsites.net/';
