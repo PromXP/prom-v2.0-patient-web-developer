8c7426b7adeb20ebcebfcc7bf07fce49d2e0464a
Inbasekar <inbatech2024@gmail.com>
2025-09-27 13:09:38 +0530

    fixed

--- src/app/Components/Dashboard.jsx
+++ src/app/Components/Dashboard.jsx
@@ -167,8 +167,6 @@ useEffect(() => {
   merged = merged.filter((q) => {
     if (!q.deadline) return false;
 
-    return true;
-
     const deadlineDate = new Date(q.deadline);
     const diffDays = (today - deadlineDate) / (1000 * 60 * 60 * 24);
 
